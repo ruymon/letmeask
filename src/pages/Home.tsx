1458334032b06d80f3b6fe290a8cad6f2de56726
ruymon <ruystfrancis@gmail.com>
2021-06-21 20:46:14 -0300

    Add SVG Assets

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import illustrationImg from '../assets/images/illustration.svg';
+
+export function Home() {
+  return (
+    <div>
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas." />
+      </aside>
+    </div>
+  )
+}
