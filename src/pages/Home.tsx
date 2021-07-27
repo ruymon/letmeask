a01cf7d7fb09035d955fb529bff0a59818706013
ruymon <ruystfrancis@gmail.com>
2021-07-27 14:20:33 -0300

    Create AuthContext

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { auth, firebase } from '../services/firebase';
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -9,17 +10,16 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
-
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider).then(result => {
-      history.push('/rooms/new');
-    });
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle();
+    };
 
-   
+    history.push('/rooms/new');   
   };
 
   return (
