f89bee5d9eeee63f6a987d6baf9c23fb70dcb88e
ruymon <ruystfrancis@gmail.com>
2021-07-28 13:39:22 -0300

    Join Room Flow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import { useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss';
+import { database } from '../services/firebase';
+
 import { Button } from '../components/Button';
+import '../styles/auth.scss';
+
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if(!user) {
@@ -19,6 +24,25 @@ export function Home() {
 
     history.push('/rooms/new');   
   };
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if(roomCode.trim() === '') {
+      return;
+    };
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()) {
+      alert('Room does not exist.');
+      return;
+    };
+
+    history.push(`/rooms/${roomCode}`);
+
+  };
+
 
   return (
     <div id="page-auth">
@@ -38,10 +62,12 @@ export function Home() {
 
           <div className="separator">ou entre em uma sala</div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             {/* TODO Add Login Icon to Join Room Button  */}
             <Button type="submit">Entrar na sala</Button>
