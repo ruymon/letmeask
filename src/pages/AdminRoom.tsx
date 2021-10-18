386082ae2f0cd11a16f0dcd2d802d5551c01af02
ruymon <ruystfrancis@gmail.com>
2021-10-18 05:03:08 -0300

    Remove not used functions in Admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,10 +1,7 @@
-import { useState, FormEvent } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { useAuth } from '../hooks/useAuth';
+// import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-
-import { database } from '../services/firebase';
 
 import logoImg from '../assets/images/logo.svg';
 
@@ -19,38 +16,11 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
   const params = useParams<RoomParams>();
-  const [newQuestion, setNewQuestion] = useState('');
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
-
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === '') {
-      return;
-    };
-
-    if(!user) {
-      throw new Error('You must be logged in!');
-    };
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar,
-      },
-      isHighlighted: false,
-      isAnswered: false,
-    };
-
-    await database.ref(`rooms/${roomId}/questions`).push(question);
-
-    setNewQuestion('');
-  };
 
   return (
     <div id="page-room">
